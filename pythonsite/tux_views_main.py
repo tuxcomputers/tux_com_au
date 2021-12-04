a082feb9cc82011f885a784f901184b6346d1ca1
Harry Phillips <harry@tux.com.au>
2021-12-04 12:34:33 +1000

    Change some variable names

--- pythonsite/tux_views_main.py
+++ pythonsite/tux_views_main.py
@@ -4,44 +4,43 @@ import datetime
 
 tux_main = Blueprint('main',__name__)
 
-def file_dtm (file_name):
-    file_dtm = ''
+def file_dtm (file_name): # Take a template file name and return the date it was last modified
     directory_path = os.getcwd()
     filename = directory_path + '/pythonsite/templates/' + file_name
-    file_dtm = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
-    file_string = file_dtm.strftime("%a %d %B %Y")
-    return file_string
-    
+    file_details = os.path.getmtime(filename)
+    file_dtm = datetime.datetime.fromtimestamp(file_details)
+    date_string = file_dtm.strftime("%d %B %Y")
+    return date_string
 
 # Home and index
 @tux_main.route('/')
 @tux_main.route('/index.html')
 def index():
     file = 'tux_index.html'
-    file_time = file_dtm(file)
-    return render_template(file, dtm=file_time)
+    file_date = file_dtm(file)
+    return render_template(file, dtm=file_date)
 
 @tux_main.route('/skills')
 def skills():
     file = 'tux_skills.html'
-    file_time = file_dtm(file)
-    return render_template(file, dtm=file_time)
+    file_date = file_dtm(file)
+    return render_template(file, dtm=file_date)
 
 @tux_main.route('/portfolio')
 def portfolio():
     file = 'tux_portfolio.html'
-    file_time = file_dtm(file)
-    return render_template(file, dtm=file_time)
+    file_date = file_dtm(file)
+    return render_template(file, dtm=file_date)
 
 @tux_main.route('/experience')
 def experience():
     file = 'tux_experience.html'
-    file_time = file_dtm(file)
-    return render_template(file, dtm=file_time)
+    file_date = file_dtm(file)
+    return render_template(file, dtm=file_date)
 
 @tux_main.route('/contact')
 def contact():
     file = 'tux_contact.html'
-    file_time = file_dtm(file)
-    return render_template(file, dtm=file_time)
+    file_date = file_dtm(file)
+    return render_template(file, dtm=file_date)
 
