76d40ced0854b38362db1cf413986f3917e67fc3
Harry Phillips <harry@tux.com.au>
2021-12-04 12:13:09 +1000

    Add the last time modified to the footer

--- pythonsite/tux_views_main.py
+++ pythonsite/tux_views_main.py
@@ -1,27 +1,47 @@
 import os, re
 from flask import Blueprint,render_template,redirect,url_for,request
+import datetime
 
 tux_main = Blueprint('main',__name__)
+
+def file_dtm (file_name):
+    file_dtm = ''
+    directory_path = os.getcwd()
+    filename = directory_path + '/pythonsite/templates/' + file_name
+    file_dtm = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
+    file_string = file_dtm.strftime("%a %d %B %Y")
+    return file_string
+    
 
 # Home and index
 @tux_main.route('/')
 @tux_main.route('/index.html')
 def index():
-    return render_template('tux_index.html')
+    file = 'tux_index.html'
+    file_time = file_dtm(file)
+    return render_template(file, dtm=file_time)
 
 @tux_main.route('/skills')
 def skills():
-    return render_template('tux_skills.html')
+    file = 'tux_skills.html'
+    file_time = file_dtm(file)
+    return render_template(file, dtm=file_time)
 
 @tux_main.route('/portfolio')
 def portfolio():
-    return render_template('tux_portfolio.html')
+    file = 'tux_portfolio.html'
+    file_time = file_dtm(file)
+    return render_template(file, dtm=file_time)
 
 @tux_main.route('/experience')
 def experience():
-    return render_template('tux_experience.html')
+    file = 'tux_experience.html'
+    file_time = file_dtm(file)
+    return render_template(file, dtm=file_time)
 
 @tux_main.route('/contact')
 def contact():
-    return render_template('tux_contact.html')
+    file = 'tux_contact.html'
+    file_time = file_dtm(file)
+    return render_template(file, dtm=file_time)
 
